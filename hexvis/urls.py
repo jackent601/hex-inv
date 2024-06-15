7dc6fbf64f8d11279cfb803079f82a2081e794ee
EC2 Default User <ec2-user@ip-172-31-43-81.eu-west-2.compute.internal>
2024-06-15 22:47:06 +0000

    ui upgrade

--- hexvis/urls.py
+++ hexvis/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("testVis", views.testVis, name="testVis"),
     path("geopd", views.geopd, name="geopd"),
     path("hexpd", views.hexpd, name="hexpd"),
     
--- hexvis/views.py
+++ hexvis/views.py
@@ -4,77 +4,75 @@ from django.conf import settings
 import os
 
 from h3 import h3
+import h3pandas
 import folium
 
 import geopandas as gpd
 import geodatasets
-import h3pandas
 
 DATA_DIR = os.path.join(settings.BASE_DIR, "hexvis", "datasets")
+NS_CENTRE=[56.42, 2.74]
+NS_ZOOM=5
 
-def visualize_hexagons(hexagons, color="red", folium_map=None):
-    """
-    hexagons is a list of hexcluster. Each hexcluster is a list of hexagons. 
-    eg. [[hex1, hex2], [hex3, hex4]]
-    """
-    polylines = []
-    lat = []
-    lng = []
-    for hex in hexagons:
-        polygons = h3.h3_set_to_multi_polygon([hex], geo_json=False)
-        # flatten polygons into loops.
-        outlines = [loop for polygon in polygons for loop in polygon]
-        polyline = [outline + [outline[0]] for outline in outlines][0]
-        lat.extend(map(lambda v:v[0],polyline))
-        lng.extend(map(lambda v:v[1],polyline))
-        polylines.append(polyline)
+# def visualize_hexagons(hexagons, color="red", folium_map=None):
+#     """
+#     hexagons is a list of hexcluster. Each hexcluster is a list of hexagons. 
+#     eg. [[hex1, hex2], [hex3, hex4]]
+#     """
+#     polylines = []
+#     lat = []
+#     lng = []
+#     for hex in hexagons:
+#         polygons = h3.h3_set_to_multi_polygon([hex], geo_json=False)
+#         # flatten polygons into loops.
+#         outlines = [loop for polygon in polygons for loop in polygon]
+#         polyline = [outline + [outline[0]] for outline in outlines][0]
+#         lat.extend(map(lambda v:v[0],polyline))
+#         lng.extend(map(lambda v:v[1],polyline))
+#         polylines.append(polyline)
     
-    if folium_map is None:
-        m = folium.Map(
-            location=[sum(lat)/len(lat), sum(lng)/len(lng)], 
-            zoom_start=13,
-            tiles='cartodbpositron')
-    else:
-        m = folium_map
-    for polyline in polylines:
-        my_PolyLine=folium.PolyLine(locations=polyline,weight=8,color=color)
-        m.add_child(my_PolyLine)
-    # Add lat/lon pop-up
-    m.add_child(folium.LatLngPopup()) 
-    return m
+#     if folium_map is None:
+#         m = folium.Map(
+#             location=[sum(lat)/len(lat), sum(lng)/len(lng)], 
+#             zoom_start=13,
+#             tiles='cartodbpositron')
+#     else:
+#         m = folium_map
+#     for polyline in polylines:
+#         my_PolyLine=folium.PolyLine(locations=polyline,weight=8,color=color)
+#         m.add_child(my_PolyLine)
+#     # Add lat/lon pop-up
+#     m.add_child(folium.LatLngPopup()) 
+#     return m
     
 
-def visualize_polygon(polyline, color):
-    polyline.append(polyline[0])
-    lat = [p[0] for p in polyline]
-    lng = [p[1] for p in polyline]
-    m = folium.Map(location=[sum(lat)/len(lat), sum(lng)/len(lng)], zoom_start=13, tiles='cartodbpositron')
-    my_PolyLine=folium.PolyLine(locations=polyline,weight=8,color=color)
-    m.add_child(my_PolyLine)
-    return m
+# def visualize_polygon(polyline, color):
+#     polyline.append(polyline[0])
+#     lat = [p[0] for p in polyline]
+#     lng = [p[1] for p in polyline]
+#     m = folium.Map(location=[sum(lat)/len(lat), sum(lng)/len(lng)], zoom_start=13, tiles='cartodbpositron')
+#     my_PolyLine=folium.PolyLine(locations=polyline,weight=8,color=color)
+#     m.add_child(my_PolyLine)
+#     return m
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def testVis(request):
-    displayContext={}
-    h3_address = h3.geo_to_h3(37.3615593, -122.0553238, 9) # lat, lng, hex resolution                                                                                                        
-    m = visualize_hexagons([h3_address])
-    displayContext["map"]=m._repr_html_()
-    displayContext["form"]="test"
-    return render(request, "testVis.html", displayContext)
-
-def getFoliumPolygonMap(df, 
+def getFoliumPolygonMapFromGeoPd(df, 
                         location=[40.70, -73.94], 
                         zoom_start=10, 
                         tiles="CartoDB positron",
                         fillColor="orange",
                         popup="BoroName"):
     """
+    Plots shape file in geopandas onto folium
     https://geopandas.org/en/stable/gallery/polygon_plotting_with_folium.html
-    plots example polygon on folium map, using geopandas db
-    GeopandasDB(Polygon) -> Folium Map object
-    df should be a WGS 84 (epsg:4326) geopandas dataframe
+        using geopandas db plots example polygon on folium map
+        GeopandasDB(Polygon) -> Folium Map object
+        df should be a WGS 84 (epsg:4326) geopandas dataframe
+        default values are for example
+    
+    Can also hex-ify shape file to plot hexes
+    https://stackoverflow.com/questions/63516948/how-to-convert-shapefile-geojson-to-hexagons-using-uber-h3-in-python
+        converts polygon to hex, then plots the hex polygons
+        df must be a geopandas dataframe
     """
     m = folium.Map(location=location, zoom_start=zoom_start, tiles=tiles)
     for _, r in df.iterrows():
@@ -88,8 +86,28 @@ def getFoliumPolygonMap(df,
         geo_j.add_to(m)
     return m
 
+def getHexifiedGeoPdFromShapeGeoPd(df, resolution = 7):
+    """
+    https://stackoverflow.com/questions/63516948/how-to-convert-shapefile-geojson-to-hexagons-using-uber-h3-in-python
+    converts polygon to hex geopandas representation
+    df must be a geopandas dataframe
+    """
+    # Resample to H3 cells
+    return df.h3.polyfill_resample(resolution)
 
-def examplePlottingPolygonFolium(datapath=None,
+def getFoliumHexifiedMapFromGeoPd(df, resolution = 7):
+    """
+    https://stackoverflow.com/questions/63516948/how-to-convert-shapefile-geojson-to-hexagons-using-uber-h3-in-python
+    converts polygon to hex, then plots the hex polygons
+    df must be a geopandas dataframe
+    """
+    # Resample to H3 cells
+    gdf_h3 = getHexifiedGeoPdFromShapeGeoPd(df, resolution=resolution)
+    # Plot H3 as polygons
+    return getFoliumPolygonMapFromGeoPd(gdf_h3)
+
+
+def getFoliumMapFromDataPath(datapath=None,
                                  hexify=False,
                                  resolution = 7,
                                  location=[40.70, -73.94], 
@@ -98,8 +116,7 @@ def examplePlottingPolygonFolium(datapath=None,
                                  fillColor="orange",
                                  popup="BoroName"):
     """
-    example
-    https://geopandas.org/en/stable/gallery/polygon_plotting_with_folium.html
+    example -> https://geopandas.org/en/stable/gallery/polygon_plotting_with_folium.html
     """
     if datapath is None:
         datapath = geodatasets.get_path("nybb")
@@ -108,72 +125,56 @@ def examplePlottingPolygonFolium(datapath=None,
     df = df.to_crs(epsg=4326)
     if hexify:
         df = df.h3.polyfill_resample(resolution)
-    return getFoliumPolygonMap(df, location=location, zoom_start=zoom_start, tiles=tiles, fillColor=fillColor, popup=popup)
+    return getFoliumPolygonMapFromGeoPd(df, location=location, zoom_start=zoom_start, tiles=tiles, fillColor=fillColor, popup=popup)
 
+def renderDisplayMapFromPath(request,
+                             datapath=None,
+                             template="hexdemo.html",
+                             hexify=False,
+                             resolution = 7,
+                             location=[40.70, -73.94],
+                             zoom_start=10,
+                             tiles="CartoDB positron",
+                             fillColor="orange",
+                             popup="BoroName"):
+    
+    m = getFoliumMapFromDataPath(datapath=datapath,
+                                 hexify=hexify,
+                                 resolution = resolution,
+                                 location=location, 
+                                 zoom_start=zoom_start, 
+                                 tiles=tiles,
+                                 fillColor=fillColor,
+                                 popup=popup)
+    
+    displayContext={}
+    displayContext["map"]=m._repr_html_()
+    displayContext["form"]="test"
+    return render(request, template, displayContext)
+    
+def index(request):
+    m = folium.Map(location=NS_CENTRE, zoom_start=NS_ZOOM, tiles="CartoDB positron")
+    return render(request, "hexdemo.html", {"map":m._repr_html_()})
 
 def geopd(request):
-    m = examplePlottingPolygonFolium()
-    
-    displayContext={}
-    displayContext["map"]=m._repr_html_()
-    displayContext["form"]="test"
-    return render(request, "testVis.html", displayContext)
-
-def getHexifiedFoliumPolygon(df, resolution = 7):
-    """
-    https://stackoverflow.com/questions/63516948/how-to-convert-shapefile-geojson-to-hexagons-using-uber-h3-in-python
-    converts polygon to hex, then plots the hex polygons
-    df must be a geopandas dataframe
-    """
-    # Resample to H3 cells
-    gdf_h3 = df.h3.polyfill_resample(resolution)
-    # Plot H3 as polygons
-    return getFoliumPolygonMap(gdf_h3)
-
-def examplePlottingHexifiedPolygonFolium():
-    """
-    example
-    https://stackoverflow.com/questions/63516948/how-to-convert-shapefile-geojson-to-hexagons-using-uber-h3-in-python
-    https://geopandas.org/en/stable/gallery/polygon_plotting_with_folium.html
-    """
-    path = geodatasets.get_path("nybb")
-    df = gpd.read_file(path)
-    # Use WGS 84 (epsg:4326) as the geographic coordinate system
-    df = df.to_crs(epsg=4326)
-    return getHexifiedFoliumPolygon(df)
+    return renderDisplayMapFromPath(request)
     
 def hexpd(request):
-    m = examplePlottingHexifiedPolygonFolium()
-    
-    displayContext={}
-    displayContext["map"]=m._repr_html_()
-    displayContext["form"]="test"
-    return render(request, "testVis.html", displayContext)
-
+    return renderDisplayMapFromPath(request, hexify=True)
 
 def geons(request):
-    m = examplePlottingPolygonFolium(datapath=os.path.join(DATA_DIR,"iho.zip"),
-                                     location=[56.42, 2.74], 
-                                     zoom_start=6,
+    return renderDisplayMapFromPath(request, datapath=os.path.join(DATA_DIR,"iho.zip"),
+                                     location=NS_CENTRE, 
+                                     zoom_start=NS_ZOOM,
                                      popup=None)
-    
-    displayContext={}
-    displayContext["map"]=m._repr_html_()
-    displayContext["form"]="test"
-    return render(request, "testVis.html", displayContext)
 
 def hexns(request):
-    m = examplePlottingPolygonFolium(datapath=os.path.join(DATA_DIR,"iho.zip"),
+    return renderDisplayMapFromPath(request, datapath=os.path.join(DATA_DIR,"iho.zip"),
+                                     location=NS_CENTRE, 
+                                     zoom_start=NS_ZOOM,
                                      hexify=True,
                                      resolution=4,
-                                     location=[56.42, 2.74], 
-                                     zoom_start=6,
                                      popup=None)
-    
-    displayContext={}
-    displayContext["map"]=m._repr_html_()
-    displayContext["form"]="test"
-    return render(request, "testVis.html", displayContext)
 
 
 
